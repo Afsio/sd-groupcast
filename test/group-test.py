11b6572d4ab074d4a5e9374f8194df842f56f889
Christian Zanger <christian_zanger@hotmail.com>
2017-05-26 18:33:17 +0200

    Fixed test imports

--- test/group-test.py
+++ test/group-test.py
@@ -10,8 +10,8 @@ class TestGroup(unittest.TestCase):
         # Gets executed before every test
         set_context()
         self.h = create_host()
-        self.p = self.h.spawn('printer', printer.Printer)
-        self.g = self.h.spawn('group', group.Group, [self.p])
+        self.p = self.h.spawn('printer', printer_s.Printer)
+        self.g = self.h.spawn('group', group_s.Group, [self.p])
 
     def tearDown(self):
         # Gets executed after every test
@@ -19,7 +19,7 @@ class TestGroup(unittest.TestCase):
 
     def test_members_leave(self):
         # Test if members join and leave correctly if not announced
-        m1 = self.h.spawn('m1', member.Member, [self.p, self.g])
+        m1 = self.h.spawn('m1', member_s.Member, [self.p, self.g])
         self.g.join(m1)
         self.g.init_start()
         self.assertTrue(len(self.g.get_members()) == 1)
